70b53dd4ff127047200afeefa1c88c1c1565b686
Luis Alexander Peraza Corobo <lpc864@inlumine.ual.es>
2024-03-30 15:32:02 +0100

    improved: copiar el contenido de una matriz a otra (dos formas)

--- Source/TDI.cpp
+++ Source/TDI.cpp
@@ -23,15 +23,20 @@
 //	}
 //}
 
-void copyMatrixWithZeros(const std::vector<std::vector<int>>& originalMatrix, C_Matrix& copyMatrix) {
+void copyMatrixWithZeros(C_Matrix& originalMatrix, C_Matrix& copyMatrix) {
 	int rows = copyMatrix.LastRow();
 	int columns = copyMatrix.LastCol();
 
-	for (int i = 1; i <= rows - 1; i++) {
-		for (int j = 1; j <= columns - 1; j++) {
-			copyMatrix(i, j) = originalMatrix[i - 1][j - 1];
+	for (int i = copyMatrix.FirstRow() + 1; i <= rows - 1; i++) {
+		for (int j = copyMatrix.FirstCol() + 1; j <= columns - 1; j++) {
+			copyMatrix(i, j) = originalMatrix(i-1, j-1);
 		}
 	}
+}
+
+void removeNoise(std::vector<std::vector<int>>& originalMatrix, C_Matrix& copyMatrix) {
+	int rows = originalMatrix.size();
+	int columns = originalMatrix[0].size();
 }
 
 //int Test(int argc, char **argv);
@@ -48,11 +53,11 @@ int main(int argc, char **argv)
 	////Mostramos la matriz asociada a la imagen con ruido
 	////image.Print(1, 3);
 
-	std::vector<std::vector<int>> originalMatrix =
+	/*std::vector<std::vector<int>> originalMatrix =
 	{
 		{1, 2, 3, 4},
 		{5, 6, 7, 8}
-	};
+	};*/
 
 	/*std::vector<std::vector<int>> copyMatrix(originalMatrix.size()+2, std::vector<int>(originalMatrix[0].size()+2,0));
 	
@@ -60,12 +65,21 @@ int main(int argc, char **argv)
 	
 	printMatrix(copyMatrix);*/
 
-	C_Matrix copyMatrix(0, originalMatrix.size() + 1, 0, originalMatrix[0].size() + 1, 0);
+	C_Matrix originalMatrix;
+
+	originalMatrix.Read("matrix.txt");
+
+	originalMatrix.Print(1, 1);
+
+	C_Matrix copyMatrix(originalMatrix.FirstRow(), originalMatrix.LastRow() + 2, originalMatrix.FirstCol(), originalMatrix.LastCol() + 2, 0);
+
+	std::cout << std::endl << std::endl;
+
+	//copyMatrix.Print(1, 1);
 
 	copyMatrixWithZeros(originalMatrix, copyMatrix);
 
 	copyMatrix.Print(1, 1);
-
 
 	return 0;
 	//return Test(argc, argv);
