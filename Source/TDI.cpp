c7de0571328fa329fcee0b0c62b21c73b8ffb826
Luis Alexander Peraza Corobo <lpc864@inlumine.ual.es>
2024-04-06 16:26:39 +0200

    PRIMERA PARTE HECHA: CALCULO MEDIANA (CODIGO MÁS LIMPIO Y DOCUMENTADO

--- Source/TDI.cpp
+++ Source/TDI.cpp
@@ -29,67 +29,72 @@ void matrixToVector(C_Matrix& matrix, std::vector<int>& vector) {
 	}
 }
 
-void removeNoise(C_Matrix& originalMatrix, C_Matrix& copyMatrix) {
-	//Definimos el vector donde volcaremos los elementos de la submatriz para su tratamiento
-	std::vector<int> vector;
-	
-	int rows = copyMatrix.LastRow();
-	int columns = copyMatrix.LastCol();
+void copiarContenidoMatriz(C_Image& imagen, C_Matrix& matrizCopiaContenido) {
+	int rows = matrizCopiaContenido.LastRow();
+	int columns = matrizCopiaContenido.LastCol();
 
-	//Iteramos hasta i <= rows - 2 y j <= colums - 2 para no considerar las filas y columnas de ceros para no desbordarnos al generar las submatrices.
-	//que usaremos para el calculo de la mediana de los pixeles que queremos. 
-	//Si rows y columns empezaran desde 0, entonces tendriamos que restar tres
-	for (int i = copyMatrix.FirstRow(); i <= rows - 2; i++) {
-		for (int j = copyMatrix.FirstCol(); j <= columns - 2; j++) {
-			//Utilizaremos una submatrix. Este submatrix no será copia de la original, entonces no trabajaremos sobre ella, pero si la usaremos 
-			//para facilitar el manejo sobre la matriz original
-			C_Matrix submatrix(copyMatrix, 1, 3, 1, 3, i, j);
-
-			matrixToVector(submatrix, vector);
-
-			bubbleSort(vector);
-
-			//Tomamos la mediana. Estamos trabajando con submatrices 3x3 para facilitar el calculo de la mediana debido que tendremos
-			//un vector de nueve elementos y entonces la mitad siempre será exactamente su tamaño entre dos 
-			originalMatrix(i, j) = vector[vector.size() / 2];
-
-			//Limpiamos el vector
-			vector.clear();
+	for (int i = matrizCopiaContenido.FirstRow() + 1; i <= rows - 1; i++) {
+		for (int j = matrizCopiaContenido.FirstCol() + 1; j <= columns - 1; j++) {
+			matrizCopiaContenido(i, j) = imagen(i-1, j-1);
 		}
 	}
 }
 
-void copyMatrixWithZeros(C_Matrix& originalMatrix, C_Matrix& copyMatrix) {
-	int rows = copyMatrix.LastRow();
-	int columns = copyMatrix.LastCol();
+void eliminarRuido(C_Image& imagen)
+{
+	//Definamos una matriz que será copia de la matriz de la imágen con la diferencia de que agregaremos sobre ella dos filas
+	//y dos columnas de ceros tanto al inicio como al final con el motivo de utilizarla para la eliminación de ruido
+	C_Matrix matrizCopiaContenido(imagen.FirstRow(), imagen.LastRow() + 2, imagen.FirstCol(), imagen.LastCol() + 2, 0);
 
-	for (int i = copyMatrix.FirstRow() + 1; i <= rows - 1; i++) {
-		for (int j = copyMatrix.FirstCol() + 1; j <= columns - 1; j++) {
-			copyMatrix(i, j) = originalMatrix(i-1, j-1);
+	//Llamemos a la función que copia el contenido de una matriz en otra, guardando las diferencias del problema
+	copiarContenidoMatriz(imagen, matrizCopiaContenido);
+
+	//Definamos el vector donde volcaremos los elementos de la futura submatriz para su tratamiento
+	std::vector<int> vector;
+
+	//Definamos ciertas variables que estaremos utilizando
+	int rows = matrizCopiaContenido.LastRow();
+	int columns = matrizCopiaContenido.LastCol();
+
+	//Las respectivas matrices empiezan desde uno, entonces iteramos hasta rows/columns - 2 para no considerar las filas y columnas de ceros
+	//agregadas al momento de reemplazar el pixel con ruido con su correspondiente valor sin ruido
+	for (int i = matrizCopiaContenido.FirstRow(); i <= rows - 2; i++) {
+		for (int j = matrizCopiaContenido.FirstCol(); j <= columns - 2; j++) {
+			//Definamos una submatrix de dimension 3x3. Esta submatrix no será copia de la original, entonces no trabajaremos sobre ella,
+			//pero si la usaremos para facilitar el manejo sobre la matriz original. La elección de dimensión 3x3 ha sido para 
+			//para facilitar el calculo de la mediana debido que tendremos un vector de nueve elementos y, entonces, la mitad
+			//siempre será exactamente su tamaño partido entre dos 
+			C_Matrix submatrix(matrizCopiaContenido, 1, 3, 1, 3, i, j);
+
+			//Volquemos los elementos de la matriz al vector
+			matrixToVector(submatrix, vector);
+
+			//Ordenemos los elementos del vector
+			bubbleSort(vector);
+
+			//Reemplazamos el valor del pixel en la posición (i,j) por el valor de la mediana correspondiente dentro de la mascara centrada
+			//en ella 
+			imagen(i, j) = vector[vector.size() / 2];
+
+			//Limpiemos el vector para futuras iteraciones
+			vector.clear();
 		}
 	}
 }
 
 int main(int argc, char **argv)
 {
-	
-	//La clase C_Image define un cuerpo para trabajar con imagenes, entonces definimos un objeto de dicha clase
-	C_Image image;
+	//La clase C_Image define un cuerpo para trabajar con imagenes, entonces definamos un objeto de dicha clase llamada "image"
+	C_Image imagen;
 
 	//Cargamos la imagen de prueba en el objeto definido con anterioridad
-	image.ReadBMP("bear_ruido.bmp");
+	imagen.ReadBMP("bear_ruido.bmp");
 
-	//Definimos una matriz copia de la original, pero con ceros
-	C_Matrix copyMatrix(image.FirstRow(), image.LastRow() + 2, image.FirstCol(), image.LastCol() + 2, 0);
+	//Llamemos a la función que elimine el ruido estilo "sal-pimienta" de la imagen
+	eliminarRuido(imagen);
 
-	//Copiamos su contenido
-	copyMatrixWithZeros(image, copyMatrix);
-
-	//Eliminamos el ruido
-	removeNoise(image, copyMatrix);
-
-	//Escribimos sobre un nuevo archivo
-	image.Write("bear_sin_ruido.bmp");
+	//Escribamos el contenido de la matriz sobre un archivo
+	imagen.Write("bear_sin_ruido.bmp");
 
 	return 0;
 }
